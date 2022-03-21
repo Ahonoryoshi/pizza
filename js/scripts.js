3807a5fd0b5288e8c8b00b9b2f7764dff5f2d6a3
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-21 19:14:26 +0300

    add code to extract prices

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ function Pizza(size,quantity,crust,topping){
     this.size = size;
     this.quantity = quantity;
     this.crust = crust;
-    this,topping = topping;
+    this.topping = topping;
 };
+
+Pizza.prototype.detaills = function(){
+    return 'You ordered ' +  this.quantity +" "+ this.size + " pizzas with " +
+    this.crust + " crust, and " + this.topping + " topping"
+}
 
 
 
@@ -41,24 +46,34 @@ $(document).ready(function(){
         var newSize = $("input[type=radio][name=size]:checked").val();
         if (newSize==="small"){
             var newQuantity = parseInt($('#qs').val());
-            var newCrust = $("input[type=radio][name=crusts]:checked").val();
-            var newTopping = $("input[type=radio][name=toppings]:checked").val();
+            var newCrust = $("input[type=radio][name=crusts]:checked").val().slice(3);
+            var newTopping = $("input[type=radio][name=toppings]:checked").val().slice(3);
+            var crustPrice = parseInt($("input[type=radio][name=crusts]:checked").val().slice(0,3));
+            var Toppingprice = parseInt($("input[type=radio][name=toppings]:checked").val().slice(0,3));
         }
         else if (newSize==="medium"){
             var newQuantity = parseInt($('#qm').val());
-            var newCrust = $("input[type=radio][name=crustm]:checked").val();
-            var newTopping = $("input[type=radio][name=toppingm]:checked").val();
+            var newCrust = $("input[type=radio][name=crustm]:checked").val().slice(3);
+            var newTopping = $("input[type=radio][name=toppingm]:checked").val().slice(3);
+            var crustPrice = parseInt($("input[type=radio][name=crustm]:checked").val().slice(0,3));
+            var Toppingprice = parseInt($("input[type=radio][name=toppingm]:checked").val().slice(0,3));
         }
         else if (newSize==="large"){
             var newQuantity = parseInt($('#ql').val());
-            var newCrust = $("input[type=radio][name=crustl]:checked").val();
-            var newTopping = $("input[type=radio][name=toppingl]:checked").val();
+            var newCrust = $("input[type=radio][name=crustl]:checked").val().slice(3);
+            var newTopping = $("input[type=radio][name=toppingl]:checked").val().slice(3);
+            var crustPrice =parseInt($("input[type=radio][name=crustl]:checked").val().slice(0,3));
+            var Toppingprice = parseInt($("input[type=radio][name=toppingl]:checked").val().slice(0,3));
         }
         else{
             alert('select size')
         }
 
-        $('#checkout').text(newSize + ", " + newQuantity +", " + newCrust + ", " + newTopping)
+        var newPizza = new Pizza(newSize,newQuantity,newCrust,newTopping);
+
+        var newPrice =
+
+        $('#checkout').text(newPizza.detaills())
         $('.details').show();
     })
 
