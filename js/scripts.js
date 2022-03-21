606bdb0170d0361670e6fe8d168b79f955450c81
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-21 18:12:37 +0300

    add if and if else statements to cater for different sizes

--- js/scripts.js
+++ js/scripts.js
@@ -32,5 +32,27 @@ $(document).ready(function(){
     })
     $("#pickup").click(function(){
         $(".location").hide();
+    });
+
+
+
+    $('#charge').click(function(){
+        var newSize = $("input[type=radio][name=size]:checked").val();
+        if (newSize==="small"){
+            var newQuantity = parseInt($('#qs').val());
+            var newCrust = $("input[type=radio][name=crusts]:checked").val();
+            var newTopping = $("input[type=radio][name=toppings]:checked").val();
+        }
+        else if (newSize==="medium"){
+            var newQuantity = parseInt($('#qm').val());
+            var newCrust = $("input[type=radio][name=crustm]:checked").val();
+            var newTopping = $("input[type=radio][name=toppingm]:checked").val();
+        }
+        else if (newSize==="large"){
+            var newQuantity = parseInt($('#ql').val());
+            var newCrust = $("input[type=radio][name=crustl]:checked").val();
+            var newTopping = $("input[type=radio][name=toppingl]:checked").val();
+        }
     })
-})
+
+});
