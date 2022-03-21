b39fda0c913d13ee883f2a8b93b9c8144e73f9fc
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-21 15:21:39 +0300

    add a place for user interface logic using .ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+//business logic
 function Pizza(size,quantity,crust,topping){
     this.size = size;
-    this.quantity = quantity
+    this.quantity = quantity;
     this.crust = crust;
     this,topping = topping;
 };
+
+
+//user-interface logic
+
+$(document).ready(function(){
+
+})
